f496054520b53efd3253f7f9415efcb4c08402cb
Kara D <lunarautomaton6@gmail.com>
2021-10-03 15:50:44 -0700

    rsiedit

--- RSIDiffBot/Program.cs
+++ RSIDiffBot/Program.cs
@@ -1,12 +1,9 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Net.Mail;
 using System.Threading.Tasks;
 using CommandLine;
-using RSIDiffBot;
+using Importer;
 using static CommandLine.Parser;
-using static RSIDiffBot.Const;
 
 namespace RSIDiffBot
 {
@@ -30,31 +27,29 @@ namespace RSIDiffBot
             NewStatePath = newStatePath;
         }
     }
-
-    public static class Const
-    {
-        public const string nl = @"%0A";
-    }
-
+    
+    // ReSharper disable once InconsistentNaming
     public static class RSIDiffBot
     {
+        // you can't just do '\n' because github actions for some reason
+        // truncates every newline..
+        public const string Nl = @"%0A";
+        
         static async Task StartDiffAsync(ActionInputs inputs)
         {
-            // you can't just do '\n' because github actions for some reason
-            // truncates every newline..
 
             Console.WriteLine($"modified {inputs.Modified}");
             Console.WriteLine($"removed {inputs.Removed}");
             Console.WriteLine($"added {inputs.Added}");
 
-            List<ChangedState> states = new List<ChangedState>()
+            var states = new List<ChangedState>()
             {
-                new ChangedState("test_state", "", "b"),
-                new ChangedState("test_state_2", "a", "b"),
-                new ChangedState("test_state_3", "b", "")
+                new ("test_state", "", "b"),
+                new("test_state_2", "a", "b"),
+                new ("test_state_3", "b", "")
             };
 
-            var title = $@"This PR makes changes to 1 or more RSIs. Here is a summary of all changes:{nl}{nl}";
+            var title = $@"This PR makes changes to 1 or more RSIs. Here is a summary of all changes:{Nl}{Nl}";
             var summary = title + WrapInCollapsible(CreateTable(states), "test.rsi");
 
             Console.WriteLine($"::set-output name=summary-details::{summary}");
@@ -62,16 +57,26 @@ namespace RSIDiffBot
             Environment.Exit(0);
         }
 
-        // Wraps some string in GitHub collapsible markdown
+        /// <summary>
+        ///     Wraps a string in a collapsible for GitHubMarkdown.
+        /// </summary>
+        /// <param name="markdown">The string to wrap</param>
+        /// <param name="title">The text to use for</param>
+        /// <returns>The wrapped string.</returns>
         static string WrapInCollapsible(string markdown, string title)
         {
-            var str = $@"<details><summary>{title}</summary>{nl}<p>{nl}{nl}" + markdown;
-            return str + $@"{nl}{nl}</p>{nl}</details>";
+            var str = $@"<details><summary>{title}</summary>{Nl}<p>{Nl}{Nl}" + markdown;
+            return str + $@"{Nl}{Nl}</p>{Nl}</details>";
         }
 
+        /// <summary>
+        ///     Creates a GitHub markdown table for a single RSI's changed states.
+        /// </summary>
+        /// <param name="states">The list of changed states.</param>
+        /// <returns>A string with a formatted markdown table.</returns>
         static string CreateTable(IEnumerable<ChangedState> states)
         {
-            var str = $@"| State | Old | New | Status{nl}| --- | --- | --- | --- |{nl}";
+            var str = $@"| State | Old | New | Status{Nl}| --- | --- | --- | --- |{Nl}";
 
             foreach (var state in states)
             {
@@ -81,7 +86,7 @@ namespace RSIDiffBot
                 else if (state.NewStatePath == String.Empty)
                     status = ModifiedType.Removed;
 
-                str += $@"| {state.Name} | {state.OldStatePath} | {state.NewStatePath} | {status}{nl}";
+                str += $@"| {state.Name} | {state.OldStatePath} | {state.NewStatePath} | {status}{Nl}";
             }
 
             return str;
