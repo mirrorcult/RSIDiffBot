0108e872ab5850c0080a1f2fff642dfc65d1f54e
Kara D <lunarautomaton6@gmail.com>
2021-10-02 21:09:44 -0700

    different args

--- RSIDiffBot/ActionInputs.cs
+++ RSIDiffBot/ActionInputs.cs
@@ -6,9 +6,19 @@ namespace RSIDiffBot
 {
     public class ActionInputs
     {
-        [Option('f', "files",
-            Required = true,
-            HelpText = "A list of changed files.")]
-        public IEnumerable<string> Files { get; set; } = null!;
+        [Option('m', "modified",
+            Required = false,
+            HelpText = "A list of modified files.")]
+        public IEnumerable<string> Modified { get; set; } = null!;
+        
+        [Option('r', "removed",
+            Required = false,
+            HelpText = "A list of removed files.")]
+        public IEnumerable<string> Removed { get; set; } = null!;
+        
+        [Option('a', "added",
+            Required = false,
+            HelpText = "A list of added files.")]
+        public IEnumerable<string> Added { get; set; } = null!;
     }
 }
